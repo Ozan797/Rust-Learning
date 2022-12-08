7f4c706ed1a6ed84a5714b12df2da54780a67dd4
Ozan Gokberk <ozan8@hotmail.co.uk>
2022-12-08 16:36:10 +0000

    hello world

--- hello.rs
+++ hello.rs
@@ -1,3 +1,3 @@
-fn main () {
-    println!("Hello, world!");
-}
+fn main () { // main is the entry point of the program
+    println!("Hello, world!"); // println! is a macro that prints a string to the console
+} // main ends here
